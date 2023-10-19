667549bc9ab9284ba80675ee2eb2442e8ecabdec
shuva79 <shuva@Shuva.localdomain>
2023-10-19 18:32:42 +0545

    new file:   00. Marco Polo/marco.sh 	new file:   00. Marco Polo/polo.sh

---
+++ 00. Marco Polo/marco.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+marco () {
+
+	current_dir=$(pwd)
+	echo "Current Directory has been saved $current_dir"
+}
---
+++ 00. Marco Polo/polo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+polo(){
+	echo "Directing to directory: $current_dir"
+	cd $current_dir
+}
+
